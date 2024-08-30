f5028afb12f00fa81c428d131477c7c7932e8db5
benjamin <benjaminatai@gmail.com>
2024-08-29 23:40:20 -0300

    Add btn,password,etc

--- src/main/java/org/myapp/login/igu/controller/MainController.java
+++ src/main/java/org/myapp/login/igu/controller/MainController.java
@@ -1,10 +1,23 @@
 package org.myapp.login.igu.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class MainController {
 
+    @FXML
+    public TextField txtUser;
+    @FXML
+    public TextField txtPassword;
+    public TextArea txtArea;
 
 
+    public void btnClean(ActionEvent actionEvent) {
+        txtUser.setText("");
+        txtPassword.setText("");
+        txtArea.setText("");
+    }
 }
